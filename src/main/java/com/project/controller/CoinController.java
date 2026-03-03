ae507414c59a72f420b882c77a88fe13b4269160
Minho Kang <busster@naver.com>
2026-03-03 14:10:56 +0900

    coin 에 대한 mvc 기초 작업

---
+++ src/main/java/com/project/controller/CoinController.java
@@ -0,0 +1,16 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/coin")
+@RequiredArgsConstructor
+public class CoinController {
+	
+	
+}
