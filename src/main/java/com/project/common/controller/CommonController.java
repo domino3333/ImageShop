093f551f9ff646ea9ff7cb1ad03f9c971e1bb480
Minho Kang <busster@naver.com>
2026-03-04 14:36:42 +0900

    CommonExceptionHandler 작성

--- src/main/java/com/project/common/controller/CommonController.java
+++ src/main/java/com/project/common/controller/CommonController.java
@@ -3,8 +3,8 @@ package com.project.common.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,12 +13,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/error")
 public class CommonController {
 	
-	@RequestMapping(value = "/errorCommon", method = RequestMethod.GET)
+	@GetMapping("/errorCommon")
 	public void handleCommonError() {
 		log.info("errorCommon");
 	}
 
-	@RequestMapping(value = "/accessError", method = RequestMethod.GET)
+	@GetMapping("/accessError")
 	public void accessDenied(Authentication auth, Model model) {
 		log.info("access Denied : " + auth);
 		model.addAttribute("msg", "Access Denied(접근 불가)");
